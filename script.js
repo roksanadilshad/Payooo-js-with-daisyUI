6db7dfbb63f94b28b0d0afdc3cefb9000fa6f63f
roksanadilshad <tanvirhossaintufa@gmail.com>
2025-08-23 23:37:06 +0600

    connect login button with main page

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 document.getElementById('form-btn')
         .addEventListener('click', function(event){
             event.preventDefault();
-     console.log(event);
+    const phoneNumValue = document.getElementById('phn-num').value;
+    const phnNumConvert = parseInt(phoneNumValue);
+
+    const passNumValue = document.getElementById('pass-num').value;
+    const passNumConvert = parseInt(passNumValue)
+    
+    const phnNum = 1234567890;
+    const passNum = 1234;
+
+    if(phnNumConvert === phnNum && passNumConvert === passNum){
+        window.location.href = "./main.html"
+    }
+    else{
+        alert("Invalid Number or pin")
+        
+    }
+
 })
